8279e29e27a1a2cd37206cfce75778eda6f90902
Nkoa Christophe <nkoachristophe@gmail.com>
2021-05-10 01:37:43 +0100

    connection to local db

--- manage_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try {
-        $db = new PDO('mysql:host=localhost;dbname=formation_users;charset=utf8',
-        'root', 'Programme@237Cameroun976');
-        die('successfull connection');
-    }catch(Exception $e) {
-        die('Erreur: '.$e->getMessage());
-    }
-?>
